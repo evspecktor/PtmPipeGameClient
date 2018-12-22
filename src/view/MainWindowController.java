57bbad0826382ee344154e6c7ff955348b209c37
Talia Mazor <tmazor@paloaltonetworks.com>
2018-12-22 18:27:44 +0200

    F instead of r

--- src/view/MainWindowController.java
+++ src/view/MainWindowController.java
@@ -1,6 +1,7 @@
 package view;
 
 import java.io.File;
+import java.io.StringWriter;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.time.LocalTime;
@@ -25,7 +26,7 @@ public class MainWindowController implements Initializable{
 	char[][] pipeData= {
 			{'s','-','-','7'},
 			{' ',' ',' ','|'},
-			{' ','r','-','J'},
+			{' ','F','-','J'},
 			{' ','L','-','g'}
 	};
 	
@@ -36,9 +37,11 @@ public class MainWindowController implements Initializable{
 	@FXML
 	private Label timerLabel;
 	
-	private String inputFromServer = "";
+	private StringWriter inputFromServer = new StringWriter();
 	
 	private int port = 6100;
+	//private String ip = "127.0.0.1";
+	private String ip = "10.0.0.3";
 	
 	private Date startTime, submitTime;
 	private Integer stepsCounter = 0;
@@ -52,17 +55,47 @@ public class MainWindowController implements Initializable{
 				System.out.println("WOHOOOO! :) ");
 				return;
 			}
-			String[] splited = s.split(",");
-			int x = Integer.parseInt(splited[0]);
-			int y = Integer.parseInt(splited[1]);
-			int rotationCount = Integer.parseInt(splited[2]);
-			
-			for (int i = 0; i < rotationCount; i ++)
-			{
-				pipesRotation(x,y);
-				Thread.sleep(10);
+			System.out.println("solve me!!" + s + "!");
+			String[] line = s.split("\n");
+			String[] splited;
+		
+			//for each line, do rotation
+			for (String string : line) {
+				System.out.println("line:" + string+"!");
+				
+				if (string.equals("done\n"))
+				{
+					System.out.println("done!!");
+					break;
+				}
+				splited = string.split(",");
+				
+				System.out.println("x :" + splited[0] + "!");
+				System.out.println("y :" + splited[1] + "!");
+				System.out.println("rotationCount :" + splited[2] + "!");
+				
+				int x = Integer.parseInt(splited[0]);
+				int y = Integer.parseInt(splited[1]);
+				int rotationCount = Integer.parseInt(splited[2]);
+				
+				System.out.println("x : " + x);
+				System.out.println("y : " + y);
+				System.out.println("rotationCount : " + rotationCount);
+				
+				
+				for (int i = 0; i < rotationCount-1; i ++)
+				{
+					System.out.println("i: " + i );
+					pipesRotation(y,x);
+					Thread.sleep(10);
+				}
+		
+				
 			}
-		}
+			
+			
+			
+				}
 		System.out.println("Server sent null");
 	}
 	
@@ -92,9 +125,9 @@ public class MainWindowController implements Initializable{
 	});
 }
 	
-	
 	private void pipesRotation(int x, int y)
 	{
+		System.out.println("--inside pipesRotation--");
 		char PipeType = pipeDisplayer.getPipe(x, y);
 		
 		switch (PipeType) {
@@ -102,11 +135,11 @@ public class MainWindowController implements Initializable{
 			pipeDisplayer.pipeBoard[x][y] = 's';
 			break;
 		case 'L':
-			pipeDisplayer.pipeBoard[x][y] = 'r';
+			pipeDisplayer.pipeBoard[x][y] = 'F';
 			pipeDisplayer.redraw();
 			setStepsCounter();
 			break;
-		case 'r':
+		case 'F':
 			pipeDisplayer.pipeBoard[x][y] = '7';
 			pipeDisplayer.redraw();
 			setStepsCounter();
@@ -182,18 +215,24 @@ public class MainWindowController implements Initializable{
 		long difference = submitTime.getTime() - startTime.getTime();
 		timerLabel.setText("Timer: " + difference/1000 + " seconds");
 		
-		Communication.start(pipeDisplayer.covertGameToString(),"10.0.0.3", port);
-		if (inputFromServer.equals("done"))
+		Communication.start(pipeDisplayer.covertGameToString(),inputFromServer,ip, port);
+		System.out.println("input from server: " + inputFromServer + "!");
+		if (inputFromServer.toString().equals("done\n"))
 		{
 			System.out.println("WOHOOOO! :) ");
 			return;
+		}
+		else
+		{
+			System.out.println("Try again ");
 		}
 	}
 	
 	public void Solve() throws InterruptedException
 	{
-		Communication.start(pipeDisplayer.covertGameToString(),"10.0.0.3", port);
-		handleServerSolution(inputFromServer);
+		Communication.start(pipeDisplayer.covertGameToString(),inputFromServer,ip, port);
+		System.out.println("evg shaming: "+ inputFromServer.toString());
+		handleServerSolution(inputFromServer.toString());
 	}
 	
 }
